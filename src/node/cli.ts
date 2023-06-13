763115b83c43fdcb3832ef428aac6cbaf3d7d410
xywang71 <xywang71@iflytek.com>
2023-06-13 09:47:27 +0800

    feat:  add lint-staged

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -5,6 +5,7 @@ import { build } from './build';
 const cli = cac('island').version('0.0.1').help();
 
 cli.command('dev [root]', 'start dev server').action(async (root: string) => {
+  console.log('start dev server');
   const server = await createDevServer(root);
   await server.listen();
   server.printUrls();
@@ -13,6 +14,7 @@ cli.command('dev [root]', 'start dev server').action(async (root: string) => {
 cli
   .command('build [root]', 'build a production')
   .action(async (root: string) => {
+    console.log('start building');
     await build(root);
   });
 
