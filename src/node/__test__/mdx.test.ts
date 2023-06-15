5e88d02f7288a294b2a95ab7390d88ea1186d350
xywang71 <xywang71@iflytek.com>
2023-06-15 19:07:57 +0800

    feat: 实现rehype的preWrapper插件

--- src/node/__test__/mdx.test.ts
+++ src/node/__test__/mdx.test.ts
@@ -3,13 +3,15 @@ import { describe, test, expect } from 'vitest';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import { rehypePluginPreWrapper } from '../plugin-mdx/rehypePlugins/preWrapper';
 
 describe('Markdown compile cases', () => {
   // init processor
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeStringify);
+    .use(rehypeStringify)
+    .use(rehypePluginPreWrapper);
 
   test('Compile title', async () => {
     const mdContent = '# Hello';
@@ -23,5 +25,20 @@ describe('Markdown compile cases', () => {
     expect(result.value).toMatchInlineSnapshot(
       '"<p>I am using <code>Island.js</code></p>"'
     );
+  });
+
+  test('Compile code block', async () => {
+    const mdContent = '```js\n console.log(123);\n```';
+    const result = processor.processSync(mdContent);
+    /**
+      <div class="language-js">
+        <span class="lang">js</span>
+        <pre><code class="language-js">console.log(123)</code></pre>
+      </div>
+     */
+    expect(result.value).toMatchInlineSnapshot(`
+      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre><code class=\\"\\"> console.log(123);
+      </code></pre></div>"
+    `);
   });
 });
