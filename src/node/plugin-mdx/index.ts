26341d3b15e76cb89730eca0959b47318881d938
xywang71 <xywang71@iflytek.com>
2023-06-16 15:44:47 +0800

    feat: 开发mdx热更新插件

--- src/node/plugin-mdx/index.ts
+++ src/node/plugin-mdx/index.ts
@@ -1,5 +1,12 @@
+import { pluginMdxHMR } from './pluginMdxHrm';
 import { pluginMdxRollup } from './pluginMdxRollup';
 
+// Vite 热更新机制
+// 1. 监听到文件变动
+// 2. 定位到热更新边界模块
+// 3. 执行更新逻辑
+
+// React 的组件的热更新方式，依赖react-fresh
 export async function createMdxPlugin() {
-  return [await pluginMdxRollup()];
+  return [await pluginMdxRollup(), pluginMdxHMR()];
 }
